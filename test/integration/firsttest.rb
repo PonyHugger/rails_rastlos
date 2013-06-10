fe173f0b8171a7dac99e20a588ab3987027c429f
Kay Schoppe <mail@kay-schoppe.de>
2013-06-10 19:04:03 +0200

    - Added new test - OSM-Check with Timeout on keyup

--- test/integration/firsttest.rb
+++ test/integration/firsttest.rb
@@ -4,6 +4,7 @@ require "test_helper"
 
 class TourCheckHeadline < ActionDispatch::IntegrationTest
   test "will the headline be displayed" do
+    visit new_tour_path
     assert new_tour_path.has_content?('New tour')
   end
 end
@@ -11,6 +12,13 @@ class CityCreatCity < ActionDispatch::IntegrationTest
   test "create new city" do
     city = City.new city_name: "Schuby"
     assert city.valid?, "valid with a city_name"
+  end
+end
+class CityCheckLonLat < ActionDispatch::IntegrationTest
+  test "create new city" do
+    city = City.new city_name: "Schuby"
+    assert city.has_content?, "9.9830831"
+	assert city.has_content?, "54.6037441"
   end
 end
 
